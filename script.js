e13b343eb161cf6fd1f1244584b3a4c7e0995aec
MarianMiranda <mirandamarianivon@outlook.es>
2023-04-12 15:59:05 -0300

    clase03 completada

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ const btn = document.querySelector("[data-form-btn]");
 console.log(btn);
 
 const createTask = (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-task]");
+    const task = document.createElement("li");
+    task.classList.add("card"); //agregar clase
+
+    input.value = "";
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task); //agregar hijo
+
+    console.log(content);
 };
 
-btn.addEventListener("click", "createTask")
+btn.addEventListener("click", createTask);
 
 
 
