a8e133b0582e01aa81c2f2e3cfb8efaec8295bc5
MarianMiranda <mirandamarianivon@outlook.es>
2023-04-12 17:16:54 -0300

    clase04 completada

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
+//Inmediately invoked function expression IIFE
+
+( () => {
+
 const btn = document.querySelector("[data-form-btn]");
-console.log(btn);
 
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
-    const list = document.querySelector("[data-task]");
+    const list = document.querySelector("[data-list]");
     const task = document.createElement("li");
     task.classList.add("card"); //agregar clase
-
     input.value = "";
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
 
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    
+    const content = 
+        `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task); //agregar hijo
 
-    console.log(content);
 };
 
 btn.addEventListener("click", createTask);
 
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
 
+    return i;
+};
+
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeICon");
+    element.classList.toggle("far");
+};
+
+}) ();
 
 
 //necesitamos un "listener" (Saber que hizo click)
